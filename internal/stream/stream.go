bec960f7dd606dc82a6079622b54377ca97f765d
weiwenchen2022 <weiwenchen2022@163.com>
2023-08-20 12:43:24 +0800

    second commit

--- internal/stream/stream.go
+++ internal/stream/stream.go
@@ -3,16 +3,18 @@ package stream
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"io"
 
 	"google.golang.org/protobuf/proto"
 )
 
-const MaxRequestBodyLen = 1<<16 - 1
+const MaxBodyLen = 1<<16 - 1
 
 // A Decoder reads and decodes Proto Buffer values from an input stream.
 type Decoder struct {
 	r *bufio.Reader
+	b []byte
 }
 
 // NewDecoder returns a new decoder that reads from r.
@@ -20,26 +22,34 @@ type Decoder struct {
 // The decoder introduces its own buffering and may
 // read data from r beyond the Proto Buffer values requested.
 func NewDecoder(r io.Reader) *Decoder {
-	return &Decoder{r: bufio.NewReaderSize(r, MaxRequestBodyLen)}
+	return &Decoder{r: bufio.NewReader(r)}
 }
+
+const minBufSize = 256
 
 // Decode reads the next ProtoBuffer-encoded value from its
 // input and stores it in the value pointed to by v.
-func (dec *Decoder) Decode(v proto.Message) error {
-	var n uint16
-	if err := binary.Read(dec.r, binary.BigEndian, &n); err != nil {
+func (dec *Decoder) Decode(m proto.Message) error {
+	var nn uint16
+	if err := binary.Read(dec.r, binary.BigEndian, &nn); err != nil {
 		return err
 	}
-	b, err := dec.r.Peek(int(n))
+
+	n := int(nn)
+	l := len(dec.b)
+	for l < n {
+		l <<= 1
+		if l < minBufSize {
+			l = minBufSize
+		}
+		dec.b = make([]byte, l)
+	}
+	_, err := io.ReadFull(dec.r, dec.b[:n])
 	if err != nil {
 		return err
 	}
-	defer func() { _, _ = dec.r.Discard(int(n)) }()
 
-	if err := proto.Unmarshal(b, v); err != nil {
-		return err
-	}
-	return nil
+	return proto.Unmarshal(dec.b[:n], m)
 }
 
 // An Encoder writes JSON values to an output stream.
@@ -49,26 +59,31 @@ type Encoder struct {
 
 // NewEncoder returns a new encoder that writes to w.
 func NewEncoder(w io.Writer) *Encoder {
-	return &Encoder{w: bufio.NewWriterSize(w, MaxRequestBodyLen)}
+	return &Encoder{w: bufio.NewWriter(w)}
 }
+
+var ErrMessageTooLarge = errors.New("message too large")
 
 // Encode writes the Proto Buffer encoding of v to the stream.
 // preceded by big-endian uint16 len.
-func (enc *Encoder) Encode(v proto.Message) error {
-	b, err := proto.Marshal(v)
+func (enc *Encoder) Encode(m proto.Message) error {
+	b, err := proto.Marshal(m)
 	if err != nil {
 		return err
 	}
 
-	var n = uint16(len(b))
-	if err := binary.Write(enc.w, binary.BigEndian, n); err != nil {
+	n := len(b)
+	if n > MaxBodyLen {
+		return ErrMessageTooLarge
+	}
+	if err := binary.Write(enc.w, binary.BigEndian, uint16(n)); err != nil {
 		return err
 	}
-	if n, err := enc.w.Write(b); err != nil || len(b) != n {
-		if err == nil {
-			err = io.ErrShortWrite
+	if wn, werr := enc.w.Write(b); n != wn {
+		if werr == nil {
+			werr = io.ErrShortWrite
 		}
-		return err
+		return werr
 	}
 	return enc.w.Flush()
 }
